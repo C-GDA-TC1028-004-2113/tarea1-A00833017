f3495b8001e69b84d467bdb16e8ab6758fce337a
A00833017 <A00833017@itesm.mx>
2021-08-17 20:12:27 +0000

    tarea1

--- assignments/09Telefono/src/exercise.py
+++ assignments/09Telefono/src/exercise.py
@@ -1,6 +1,10 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    nm =int(input("Dame el número de mensajes: "))
+    nm2 = float(input("Dame el número de megas: "))
+    nm3= int(input("Dame el número de minutos: "))
+    ct = (nm+nm2+nm3)*.8
+    print("El costo mensual es:", ct)
 
 
 
